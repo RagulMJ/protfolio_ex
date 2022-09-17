be7f16186c4d8b72cc9b3562f4b12c11042c08ff
RagulMJ <98083899+RagulMJ@users.noreply.github.com>
2022-09-17 22:07:23 +0530

    pf

--- index.js
+++ index.js
@@ -41,6 +41,20 @@ const myFunc = (navCondition) => {
   }
 }
 
+//hover effect
+const elements = document.getElementsByClassName('alpha');
+
+        for (let i = 0; i<=elements.length; i++)
+        {
+            elements[i].addEventListener('animationend',function(e){
+                elements[i].classList.remove('animated');
+            });
+
+            elements[i].addEventListener('mouseover',function(e){
+                elements[i].classList.add('animated')
+            })
+        }
+
 // Part 1 javascript functionality ends here
 $(document).ready(function () {
   if (
